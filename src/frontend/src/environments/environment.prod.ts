8adbc6c9ad27c48098581f704b2a8b416924fae7
Janek Fellien <jan.fellien@devcrowd.de>
2023-04-29 22:15:23 +0200

    fixed env values for deployment

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  clientId: '08c54b62-56cb-432e-9406-057c55dbd97a',
-  authority: 'https://devCrowdB2C.b2clogin.com/devCrowdB2C.onmicrosoft.com/B2C_1_signin',
+  clientId: 'b096cef4-7241-4cda-9e52-9f9f81cc2c25',
+  authority: 'https://devcrowdb2c.b2clogin.com/devcrowdb2c.onmicrosoft.com/B2C_1_signin',
   authorityDomain: 'devCrowdB2C.b2clogin.com',
   redirectUri: 'https://kugel-personal-search.devcrowd.de',
   endpoint: 'https://kugel-personal-search.devcrowd.de/api',
   pathSearch: '/search',
-  pathPerson: '/person',
-  endpointScopes: [ 'https://devCrowdB2C.onmicrosoft.com/67ed75a2-3ff2-46da-8dae-ebe4f34f8901/all' ]
+  pathStaffMembers: '/staff-members',
+  endpointScopes: [ 'https://devCrowdB2C.onmicrosoft.com/ab2d9786-4e2c-43e8-baf9-ff468ec0cb26/all' ]
 };
