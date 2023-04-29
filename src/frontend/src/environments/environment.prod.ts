57cd0aec9e3f17e242a8986b2c203cd05eb42775
Janek Fellien <jan.fellien@devcrowd.de>
2023-04-29 23:24:23 +0200

    updated env setting

--- src/frontend/src/environments/environment.prod.ts
+++ src/frontend/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   clientId: 'b096cef4-7241-4cda-9e52-9f9f81cc2c25',
   authority: 'https://devcrowdb2c.b2clogin.com/devcrowdb2c.onmicrosoft.com/B2C_1_signin',
   authorityDomain: 'devCrowdB2C.b2clogin.com',
-  redirectUri: 'https://kugel-personal-search.devcrowd.de',
-  endpoint: 'https://kugel-personal-search.devcrowd.de/api',
+  redirectUri: 'https://staff-search.devcrowd.de',
+  endpoint: 'https://staff-search.devcrowd.de/api',
   pathSearch: '/search',
   pathStaffMembers: '/staff-members',
   endpointScopes: [ 'https://devCrowdB2C.onmicrosoft.com/ab2d9786-4e2c-43e8-baf9-ff468ec0cb26/all' ]
