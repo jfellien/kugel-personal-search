f974543d83a95f99a5640b694a47e84286ff2b4c
Janek Fellien <jan.fellien@devcrowd.de>
2023-04-29 20:38:06 +0200

    added some seed data

--- src/backend/Kugel.StaffSearch.Database.SqlServer/KugelStaffSearchContext.cs
+++ src/backend/Kugel.StaffSearch.Database.SqlServer/KugelStaffSearchContext.cs
@@ -5,7 +5,34 @@ namespace Kugel.StaffSearch.Database.SqlServer;
 
 public class KugelStaffSearchContext : DbContext
 {
-    public KugelStaffSearchContext(DbContextOptions<KugelStaffSearchContext> options) : base(options) { }
-    
+    public KugelStaffSearchContext(DbContextOptions<KugelStaffSearchContext> options) : base(options)
+    {
+    }
+
     public DbSet<StaffMember>? StaffMember { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<StaffMember>().HasData(new StaffMember
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Petra",
+                LastName = "Bauknecht",
+                PersonalId = "2023-01-01-01"
+            },
+            new StaffMember
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Philipp",
+                LastName = "Bauknecht",
+                PersonalId = "2023-01-01-02"
+            },
+            new StaffMember
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Janek",
+                LastName = "Fellien",
+                PersonalId = "2023-01-01-03"
+            });
+    }
 }
